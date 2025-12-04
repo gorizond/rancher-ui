006fa6b3d8524e5cd24878bca0fcddbc8ccddd0c
Negash <i@negash.ru>
2025-12-05 00:22:49 +0500

    Fix status column path for Rancher 2.13

--- pkg/gorizond-ui/index.ts
+++ pkg/gorizond-ui/index.ts
@@ -1,53 +1,54 @@
-import { importTypes } from '@rancher/auto-import';
-import { IPlugin, TableColumnLocation } from '@shell/core/types';
+import { importTypes } from "@rancher/auto-import";
+import { IPlugin, TableColumnLocation } from "@shell/core/types";
 import routes from "./routes";
 
 // Init the package
 export default function (plugin: IPlugin): void {
-    importTypes(plugin);
+  importTypes(plugin);
 
-    // Provide plugin metadata from package.json
-    plugin.metadata = require('./package.json');
+  // Provide plugin metadata from package.json
+  plugin.metadata = require("./package.json");
 
-    // Load a product
-    plugin.addProduct(require('./product'));
-    // => => => Add Vue Routes
-    plugin.addRoutes(routes);
+  // Load a product
+  plugin.addProduct(require("./product"));
+  // => => => Add Vue Routes
+  plugin.addRoutes(routes);
 
-    plugin.addTableColumn(
-        TableColumnLocation.RESOURCE,
-        {resource: ['provisioning.gorizond.io.cluster']},
-        {
-            name: 'provisioning',
-            labelKey: 'gorizond.status',
-            getValue: (row: any) => {
-                return row.status.provisioning;
-            },
-            width: 100,
-            sort: ['stateSort', 'nameSort'],
-            search: ['stateSort', 'nameSort'],
-        }
-    );
+  plugin.addTableColumn(
+    TableColumnLocation.RESOURCE,
+    { resource: ["provisioning.gorizond.io.cluster"] },
+    {
+      name: "provisioning",
+      labelKey: "gorizond.status",
+      label: "Status",
+      value: "status.provisioning",
+      getValue: (row: any) => {
+        return row.status.provisioning;
+      },
+      width: 100,
+      sort: ["stateSort", "nameSort"],
+      search: ["stateSort", "nameSort"],
+    }
+  );
 
-    plugin.addTableColumn(
-        TableColumnLocation.RESOURCE,
-        { resource: ['provisioning.gorizond.io.cluster'] },
-        {
-            name:      'billing',
-            label:     'Billing',
-            value:     'status.billing',
-            sort:      ['status.billing', 'spec.billing'],
-            formatter: 'GorizondBillingFormatter',
-        }
-    );
+  plugin.addTableColumn(
+    TableColumnLocation.RESOURCE,
+    { resource: ["provisioning.gorizond.io.cluster"] },
+    {
+      name: "billing",
+      label: "Billing",
+      value: "status.billing",
+      sort: ["status.billing", "spec.billing"],
+      formatter: "GorizondBillingFormatter",
+    }
+  );
 
-    // remove cluster management icon
-    const style = document.createElement('style');
-    style.id = 'gorizond-hide-cluster-link-style';
-    style.innerHTML = `
+  // remove cluster management icon
+  const style = document.createElement("style");
+  style.id = "gorizond-hide-cluster-link-style";
+  style.innerHTML = `
     a[href="/c/_/manager/provisioning.cattle.io.cluster"] {
       display: none !important;
     }`;
-    document.head.appendChild(style);
+  document.head.appendChild(style);
 }
-
