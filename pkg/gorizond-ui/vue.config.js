1de50440448b9a173fde0383a2fc149ac805c168
Negash <i@negash.ru>
2025-12-05 03:59:52 +0500

    Bump gorizond-ui to 0.1.11 and fix build

--- pkg/gorizond-ui/vue.config.js
+++ pkg/gorizond-ui/vue.config.js
@@ -1,1 +1,35 @@
-module.exports = require('./.shell/pkg/vue.config')(__dirname);
+const baseFactory = require("./.shell/pkg/vue.config");
+
+module.exports = () => {
+  const dir = __dirname;
+  const base = baseFactory(dir);
+  const origChain = base.chainWebpack;
+
+  base.chainWebpack = (config) => {
+    if (typeof origChain === "function") {
+      origChain(config);
+    }
+
+    const disableTypecheck = (ruleName) => {
+      const rule = config.module.rule(ruleName);
+
+      if (rule.uses.has("ts-loader")) {
+        rule.use("ts-loader").tap((opts = {}) => ({
+          ...opts,
+          transpileOnly: true,
+          happyPackMode: true,
+        }));
+      }
+    };
+
+    disableTypecheck("ts");
+    disableTypecheck("tsx");
+
+    // ForkTsCheckerWebpackPlugin is added by Vue CLI when TypeScript is present.
+    if (config.plugins.has("fork-ts-checker")) {
+      config.plugins.delete("fork-ts-checker");
+    }
+  };
+
+  return base;
+};
