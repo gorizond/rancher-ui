3dfea7b1f47a9bc8342fe4ee2a132c1a9a9cfa90
Negash <i@negash.ru>
2025-12-05 04:34:19 +0500

    Dev build: disable TS checker, add diff-match-patch alias

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,52 @@
-const config = require('@rancher/shell/vue.config'); // eslint-disable-line @typescript-eslint/no-var-requires
+const config = require("@rancher/shell/vue.config"); // eslint-disable-line @typescript-eslint/no-var-requires
 
-module.exports = config(__dirname, {
-  excludes: [],
-  // excludes: ['fleet', 'example']
-});
+module.exports = (env, argv) => {
+  const base = config(__dirname, {
+    excludes: [],
+    // excludes: ['fleet', 'example']
+  });
+
+  const origChain = base.chainWebpack;
+
+  base.chainWebpack = (webpackConfig) => {
+    if (typeof origChain === "function") {
+      origChain(webpackConfig);
+    }
+
+    const disableTypecheck = (ruleName) => {
+      const rule = webpackConfig.module.rule(ruleName);
+
+      if (rule.uses.has("ts-loader")) {
+        rule.use("ts-loader").tap((opts = {}) => ({
+          ...opts,
+          transpileOnly: true,
+          happyPackMode: true,
+        }));
+      }
+    };
+
+    disableTypecheck("ts");
+    disableTypecheck("tsx");
+
+    if (webpackConfig.plugins.has("fork-ts-checker")) {
+      webpackConfig.plugins.delete("fork-ts-checker");
+    }
+  };
+
+  const origConfigure = base.configureWebpack;
+
+  base.configureWebpack = (webpackConfig = {}) => {
+    if (typeof origConfigure === "function") {
+      origConfigure(webpackConfig);
+    } else if (origConfigure && typeof origConfigure === "object") {
+      Object.assign(webpackConfig, origConfigure);
+    }
+
+    webpackConfig.resolve = webpackConfig.resolve || {};
+    webpackConfig.resolve.alias = webpackConfig.resolve.alias || {};
+    webpackConfig.resolve.alias["diff_match_patch"] =
+      require.resolve("diff-match-patch");
+  };
+
+  return base;
+};
