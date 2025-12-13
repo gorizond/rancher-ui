83242945ae97959a4c041f1afb10b4f956a0927a
Negash <i@negash.ru>
2025-12-13 18:10:13 +0500

    Add user id to payment payload (0.1.27)

--- pkg/gorizond-ui/dialog/TopUpBalanceDialog.vue
+++ pkg/gorizond-ui/dialog/TopUpBalanceDialog.vue
@@ -1,71 +1,77 @@
 <template>
   <div class="top-up-dialog p-20">
     <Banner color="info" class="mb-20">
-      Replenishment of the balance <b>{{ namespace }} / {{ name }}</b>.
+      Replenishment of the balance <b>{{ namespace }} / {{ name }}</b
+      >.
     </Banner>
 
     <form @submit.prevent="submit">
       <LabeledInput
-          v-model:value="amount"
-          class="mb-20"
-          label="Amount"
-          required
-          type="number"
-          min="1"
+        v-model:value="amount"
+        class="mb-20"
+        label="Amount"
+        required
+        type="number"
+        min="1"
       />
 
       <div class="actions">
-        <button type="button" class="btn btn-primary" @click="close">Close</button>
-        <button type="submit" class="btn role-tertiary">Top up on Yookassa</button>
+        <button type="button" class="btn btn-primary" @click="close">
+          Close
+        </button>
+        <button type="submit" class="btn role-tertiary">
+          Top up on Yookassa
+        </button>
       </div>
     </form>
   </div>
 </template>
 
-
 <script>
-import { LabeledInput } from '@components/Form/LabeledInput';
-import { Banner } from '@components/Banner';
+import { LabeledInput } from "@components/Form/LabeledInput";
+import { Banner } from "@components/Banner";
 
 export default {
   components: { LabeledInput, Banner },
   props: {
     name: { type: String, required: true },
-    namespace: { type: String, required: true }
+    namespace: { type: String, required: true },
   },
   data() {
-    return { amount: '' };
+    return { amount: "" };
   },
   methods: {
     close() {
-      this.$emit('close');
+      this.$emit("close");
     },
     async submit() {
-      const setting = await this.$store.dispatch('management/find', {
-        type: 'management.cattle.io.setting',
-        id: 'gorizond-install-payment-url'
+      const setting = await this.$store.dispatch("management/find", {
+        type: "management.cattle.io.setting",
+        id: "gorizond-install-payment-url",
       });
       const pay_url = setting.value;
-      this.openPostInNewTab(
-          pay_url + `/payment`,
-          {
-            namespace: this.namespace,
-            name: this.name,
-            amount: this.amount
-          }
-      );
+      const userId =
+        this.$store.getters["auth/v3User"]?.id ||
+        this.$store.getters["auth/principalId"] ||
+        "";
+      this.openPostInNewTab(pay_url + `/payment`, {
+        namespace: this.namespace,
+        name: this.name,
+        amount: this.amount,
+        userId,
+      });
       this.close();
     },
 
     openPostInNewTab(action, data) {
-      const form = document.createElement('form');
-      form.method = 'POST';
+      const form = document.createElement("form");
+      form.method = "POST";
       form.action = action;
-      form.target = '_blank';
+      form.target = "_blank";
 
       Object.entries(data).forEach(([key, value]) => {
-        const input = document.createElement('input');
-        input.type = 'hidden';
+        const input = document.createElement("input");
+        input.type = "hidden";
         input.name = key;
         input.value = String(value);
         form.appendChild(input);
@@ -74,7 +80,7 @@ export default {
       document.body.appendChild(form);
       form.submit();
       document.body.removeChild(form);
-    }
-  }
+    },
+  },
 };
 </script>
